0f3dd4bd461ccdd78e8dc7b65bb523803d20f063
tomtylorr@gmail.com <tomtylorr@gmail.com>
2022-05-28 12:22:19 +0300

    feat: finished about me

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+	let letter2 = 0;
+	const text = "Привет, меня зовут";
+	function typeText() {
+		if (letter2 < text.length) {
+			document.getElementById("header_about-title-js").innerHTML += text.charAt(letter2);
+			letter2++;
+			let speed = Math.floor(Math.random() * 150) + 50;
+			setTimeout(typeText, speed);
+		}
+	}
+	typeText();
+});
+
+
+
+let letter = 0;
+const name2 = "Дима!";
+function typeText2() {
+	if (letter < name2.length) {
+		document.getElementById("header_about-title__underline-js").innerHTML += name2.charAt(letter);
+		letter++;
+		let speed = Math.floor(Math.random() * 1500) + 50;
+		setTimeout(typeText2, speed);
+	}
+}
+
+typeText2();
+
