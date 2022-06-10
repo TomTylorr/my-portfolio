181669e490971837a16a901fde120ed705598650
tomtylorr@gmail.com <tomtylorr@gmail.com>
2022-06-10 18:08:33 +0300

    feat: сделал описание каждой работы своей

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,18 +42,18 @@ designContents.addEventListener('click', function (e) {
 
 		for (let i = 0; i < length; i++) {
 			let listElement = list[i].firstChild;
-			if (listElement.classList.contains("tab-link__active")){
+			if (listElement.classList.contains("tab-link__active")) {
 				listElement.classList.remove("tab-link__active")
 			}
-			content[i].classList.add("display-none")	
+			content[i].classList.add("display-none")
 		}
 
 		let currentDataAtr = tab.dataset.content;
 
 		for (let i = 0; i < length; i++) {
-			if (content[i].classList.contains(currentDataAtr)){
+			if (content[i].classList.contains(currentDataAtr)) {
 				content[i].classList.remove("display-none");
-			}	
+			}
 		}
 
 		currentElement = e.target;
@@ -61,6 +61,96 @@ designContents.addEventListener('click', function (e) {
 	}
 });
 
+const myWorksVideos = document.querySelector('.my-works_videos');
+const myWorksVideos1C = document.querySelector('.my-works_videos1C');
+const myWorksDesign = document.querySelector('.my-works_design-items');
+const myWorksVideoWrappper = document.querySelector('.my-works_video-wrappper');
+const overlay = document.querySelector('.my-works_overlay');
+const myWorksOverlayTitle = document.querySelector('.my-works_overlay-title');
+const myWorksOverlayText = document.querySelector('.my-works_overlay-text');
+const myWorksOverlayLink = document.querySelector('.my-works_overlay-link');
+
+let videoCurrentID,
+	videoCurrent;
+
+myWorksVideos.addEventListener('mouseover', function (e) {
+	if (e.target.id == "my-works_video") {
+		videoCurrentID = e.target.offsetParent.id;
+		videoCurrent = document.getElementById(videoCurrentID)
+		mouseover(videoCurrent);
+		myWorksOverlayLink.classList.remove("display-none");		
+	}
+});
+
+myWorksVideos.addEventListener('mouseleave', function (e) {
+	overlay.classList.add("display-none");
+});
+
+myWorksVideos1C.addEventListener('mouseover', function (e) {
+	if (e.target.id == "my-works_video") {
+		videoCurrentID = e.target.offsetParent.id;
+		videoCurrent = document.getElementById(videoCurrentID)
+		mouseover(videoCurrent);
+		myWorksOverlayLink.classList.add("display-none");	
+	}
+});
+
+myWorksVideos1C.addEventListener('mouseleave', function (e) {
+	overlay.classList.add("display-none");
+});
 
 
-	
+myWorksDesign.addEventListener('mouseover', function (e) {
+	if (e.target.id == "my-works_design_img") {
+		videoCurrentID = e.target.parentElement.id;
+		videoCurrent = document.getElementById(videoCurrentID)
+		mouseover(videoCurrent);
+		myWorksOverlayLink.classList.add("display-none");		
+	}
+});
+
+myWorksDesign.addEventListener('mouseleave', function (e) {
+	overlay.classList.add("display-none");
+});
+
+
+function mouseover(videoCurrent) {
+	myWorksOverlayTitle.innerHTML = data[videoCurrentID][0];
+	myWorksOverlayText.innerHTML = data[videoCurrentID][1];
+	myWorksOverlayLink.href = data[videoCurrentID][2];
+	videoCurrent.appendChild(overlay);
+	overlay.classList.remove("display-none");
+}
+
+let data = {
+	"video1": ['Учебный проект', "CSS, HTML, JS, FlexBox, Grid", "#"],
+	"video2": ['Производство кухонных фасадов', "WordPress + WooCommerce", "https://fasad-mg.by"],
+	"video3": ['Сайт был на Deal.by', "WordPress, Bootstrap4", "https://микрон.бел"],
+	"video4": ['RS-School', "CSS, HTML, JS, FlexBox, Grid,<br> Смена языка и фона", "#"],
+	"video5": ['Новое производство', "CSS, HTML, JS, FlexBox", "#"],
+	"video6": ['Сайт на Украину', "WordPress + WooCommerce", "https://sklu.net"],
+	"video1c_1": ['Внешняя обработка', "Читаю прайс  в CSV и XML формате, строю иерархию, создаю номенклатуру, создаю свойства для выгрузки на сайт, поднимаю свойства", "#"],
+	"video1c_2": ['Регламентное задание', "Скачиваю архив, распаковываю, читаю XML и загружаю: количество, цены, фото в 1С для уже существующих товаров, создаю новые карточки, если их не было", "#"],
+	"photo-desing1": ['Каталог', "Делал каталог с 0 на 32 страницы, вплоть до вывода в печать.", "вавав"],
+	"photo-desing2": ['Коммерческое', "Разработал коммерческое на фирму, которая занимается кухонными фасадами", "вавав"],
+	"photo-desing3": ['Прайс-лист', "Прайс-лит в Украину", "вавав"],
+	"photo-desing4": ['Коммерческое', "Листовка или коммерческое в Польшу", "вавав"],
+	"photo-desing5": ['Визитка', "Визка для брата Барбера", "вавав"],
+	"photo-desing6": ['Визитка', "Компания ООО 'Албико-групп'", "вавав"],
+	"photo-desing7": ['Стенд', "Стенд по охране труда на фирму", "вавав"],
+	"photo-desing8": ['VK', "Пост в группу VK", "вавав"],
+	"photo-desing9": ['Листовка', "На выставку в Украинувава", "вавав"],
+	"photo-desing10": ['Email-рассылка', "Рассылка email через сервис Unisender", "вавав"],
+	"photo-desing11": ['Логотип', "Логотип для интернет-магазина", "вавав"],
+	"photo-desing12": ['VK', "Пост в VK", "вавав"],
+	"photo-desing13": ['Печать на двери', "График работы на дверь фирмы", "вавав"],
+	"photo-desing14": ['Логотип', "Логотип для производственной фирмы", "вавав"],
+	"photo-desing15": ['Логотип', "Логотип для фирмы в Украину", "вавав"],
+	"photo-desing16": ['Шапка в FaceBook', "Шапка для группы в FaceBook", "вавав"],
+} 
+
+
+
+
+
+
